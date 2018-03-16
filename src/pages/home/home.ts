98fe0c508685b3876b0057073093745c10612a28
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2018-03-16 00:41:11 -0500

    Import HttpClientModule

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { WeatherProvider } from '../../providers/weather/weather';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private weatherProvider: WeatherProvider
+  ) {
 
   }
 
