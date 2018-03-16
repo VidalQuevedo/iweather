51aae2dc8bea7cbde9703d0e1e3f9bd1b4e45add
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2018-03-16 01:00:51 -0500

    Get and display weather data on home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,11 +9,37 @@ import { WeatherProvider } from '../../providers/weather/weather';
 })
 export class HomePage {
 
+  weather: any;
+  location: {
+    city: string,
+    state: string
+  }
+
   constructor(
     public navCtrl: NavController,
     private weatherProvider: WeatherProvider
-  ) {
+  ) { }
 
+  ionViewWillEnter() {
+    
+    // Set default location
+    this.location = {
+      city: 'Madison',
+      state: 'WI'
+    }
+
+    // Get weather
+    this.getWeather();
   }
 
+  /**
+   * Get weather
+   */
+  getWeather() {
+    this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(
+      (data => {
+        this.weather = data.current_observation;
+      })
+    );    
+  }
 }
